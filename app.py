625ba945d2c3453542d2f2532b9a7b67116c17d3
tsubedy <tsubedy@gmail.com>
2021-12-09 10:39:19 -0800

    update 12-8

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+
 import warnings
 warnings.filterwarnings('ignore')
 import time
@@ -16,42 +21,55 @@ import pymongo
 #---------------------------------#
 # Page layout
 ## Page expands to full width
-st.set_page_config(page_title='Time Series forcasting App',
+st.set_page_config(page_title='Time Series forecasting App',
     layout='wide')
 
-st.write("""
-# Time Series Forcasting App 
-**US COVID-19 cases** dataset is used to train the time series ARIMA model.
-The following table shows the future forcasts of the number of days selected. Because of the datasize, it may take longer time to process the data if you choose too many days. 
-""")
 
-#---------------------------------#
-# importing dataset
-client = pymongo.MongoClient("mongodb+srv://tsubedy:TS24751@cluster1.ppbek.mongodb.net/?retryWrites=true&w=majority")
-db = client["us_cases_db"]
-collection = db.us_cases
-cursor = collection.find()
-cases_data = list(cursor)
-us_cases_data = pd.DataFrame(cases_data, columns = ['submission_date', 'new_case'])
+st.write(""" # Time Series Forecasting App""") 
+st.write(""" ## **US COVID-19 Cases and Death** """)
 
-# Changing datatype for 'date'
-us_cases_data["date"]=pd.to_datetime(us_cases_data["submission_date"])
-us_cases_data = us_cases_data.drop(columns =['submission_date'])
+st.sidebar.title("Select Dataset ")
+st.markdown('<style>body{background-color: lightblue;}<style>', unsafe_allow_html=True)
 
-# Changing datatype for new_case
-us_cases_data = us_cases_data.astype({'new_case': 'float64'})
-us_cases_data_datewise = us_cases_data.groupby(["date"]).agg({"new_case":'sum'})
 
-us_cases_data_datewise["days_since_case"]= us_cases_data_datewise.index - us_cases_data_datewise.index[0]
-us_cases_data_datewise["days_since_case"]= us_cases_data_datewise["days_since_case"].dt.days
-us_cases_data_datewise['days_since_case'] = pd.DataFrame(us_cases_data_datewise, columns =['days_since_case'])
+def load_data():
+    # importing dataset
+    client = pymongo.MongoClient("mongodb+srv://tsubedy:TS24751@cluster1.ppbek.mongodb.net/?retryWrites=true&w=majority")
+    db = client["us_cases_db"]
+    collection = db.us_cases
+    cursor = collection.find()
+    cases_data = list(cursor)
+    us_cases_data = pd.DataFrame(cases_data, columns = ['submission_date', 'new_case', 'new_death'])
+
+    # Changing datatype for 'date'
+    us_cases_data["date"]=pd.to_datetime(us_cases_data["submission_date"])
+    us_cases_data = us_cases_data.drop(columns =['submission_date'])
+
+    # Changing datatype for new_case
+    us_cases_data = us_cases_data.astype({'new_case': 'float64', 'new_death': 'float64'})
+    us_data_datewise = us_cases_data.groupby(["date"]).agg({"new_case":'sum', "new_death":'sum'})
+
+    us_data_datewise["days_since_case"]= us_data_datewise.index - us_data_datewise.index[0]
+    us_data_datewise["days_since_case"]= us_data_datewise["days_since_case"].dt.days
+    us_data_datewise['days_since_case'] = pd.DataFrame(us_data_datewise, columns =['days_since_case'])
+
+    return us_data_datewise
+
+us_data_datewise = load_data()
+
+# Selection options
+datasets = st.sidebar.selectbox('Forecasting for', ('Cases_data', 'Death_data' ))
+
+if datasets == 'Cases_data':
+    X = us_data_datewise['new_case']
+elif datasets == 'Death_data':
+    X = us_data_datewise['new_death']
 
 # Model building
+# X = case_data.values[:,0]
 
-X = us_cases_data_datewise.values[:,0]
 train_size = int(len(X) * 0.66)
 train, test = X[0:train_size], X[train_size:len(X)]
-
 data = [x for x in train]
 predictions = []
 
@@ -66,17 +84,27 @@ for t in range(len(test)):
     data.append(obs)
     # st.write('predicted=%f, expected=%f' % (AR_pred, obs))
 
+# Plotting the predicted against test data
+
+fig=px.scatter(x=test, y=predictions)
+fig.update_layout(title="ARIMA Predicton vs Test", width=450, height=250)
+st.plotly_chart(fig)
+
+
 # evaluating predictions 
 rmse = sqrt(mean_squared_error(test, predictions))
 st.write('ARIMA Test RMSE: %.3f' % rmse)
 
 new_date=[]
-n = st.number_input("Enter the number of days (no decimal) to see the model forecast")
+n = st.number_input("Enter number of days (no decimal) to get the model forecast")
+
+
 
 for i in range(1,int(n)+1):
-    new_date.append(us_cases_data_datewise.index[-1]+timedelta(days=i))
+    new_date.append(us_data_datewise.index[-1]+timedelta(days=i))
 
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 model_forecast=pd.DataFrame(zip(new_date,predictions),
                                columns=["Dates","ARIMA forecast"])
 st.table(model_forecast)
+
